fa4cd28f42e4723a76f8e4ae11c8e794c5bbf86c
markrudenkov <markrudenkov@gmail.com>
2016-12-19 19:24:56 +0200

    OAuth2Config added

---
+++ src/main/java/com/components/admin/controller/AdminController.java
@@ -0,0 +1,7 @@
+package com.components.admin.controller;
+
+/**
+ * Created by mark on 16.12.19.
+ */
+public class AdminController {
+}
---
+++ src/main/java/com/components/student/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.components.student.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@RestController
+public class StudentController {
+
+}
